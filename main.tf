a4cb9680cc1cd0e8d86bf8a8caef7668bc979c46
Deadive <post_for_ken@hotmail.com>
2023-03-29 16:05:28 -0400

    changed organisation to unixworld

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "learn-terraform-github-actions"
+    organization = "unixworld"
 
     workspaces {
       name = "learn-terraform-github-actions"
